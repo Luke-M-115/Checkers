722782faeada14462d4d84a6528a98b4c1685087
Luke Moisan <moisanluke.lm@gmail.com>
2020-11-08 20:00:48 -0500

    adding frame for move checker

---
+++ moves.py
@@ -0,0 +1,20 @@
+def possible_moves(row,col,color):  
+    if color == 0:
+
+
+
+        print("moves calculated for red ",row," ",col)
+        places = " Possible Places: "
+        return places
+
+
+
+
+
+    elif color == 1: 
+
+
+
+        print("moves calcualted for black ",row," ",col)
+        places = " Possible Places: "
+        return places
